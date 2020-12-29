28d38d7fa5c331b0fbbbb3ee5f3f887275264895
Lisa <bashir.lisa@yahoo.com>
2020-12-29 19:48:13 +0500

    initial

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.post( "/", function( req, res ) {
     var email = req.body.email;
     var fname = req.body.firstName;
     var lname = req.body.lastName;
-    console.log( req.body.firstName );
-    console.log( req.body.lastName );
-    console.log( req.body.email );
+    console.log( fname );
+    console.log( lname );
+    console.log( email );
 
     var data = {
         members: [
@@ -64,15 +64,6 @@ app.post( "/failure", function( req, res ) {
     res.redirect( "/" );
 });
 
-// curl -X POST \
-//   https://server.api.mailchimp.com/3.0/lists \
-//   -H 'authorization: Basic <USERNAME:PASSWORD>' \
-//   -d '{"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"visibility":"pub","double_optin":false,"marketing_permissions":false}'
-
-
 app.listen( process.env.PORT || 3000, function() {
     console.log("  3000 listening....");
 })
-
-//  aa533ba8e06335eb4a667b4fa35681f2-us7
-//  68ffac6cd6
